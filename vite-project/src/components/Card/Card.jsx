19ada225084a357cb06587e80ea45979bda702bc
Kristoffergidd <72502045+Kristoffergidd@users.noreply.github.com>
2024-04-15 22:39:39 +0200

    g

--- vite-project/src/components/Card/Card.jsx
+++ vite-project/src/components/Card/Card.jsx
@@ -71,22 +71,21 @@ import vendorNinja from "../../assets/vendor-ninja.svg";
 import chipImage from '../../assets/chip-dark.svg';
 import vendorEvil from '../../assets/vendor-evil.svg';
 import vendorBlockchain from '../../assets/vendor-blockchain.svg';
-import './Card.css';
 
 const Card = () => {
   const cards = useSelector(state => state.card.cards);
   const dispatch = useDispatch();
 
   const handleRemoveCard = (index) => {
-    dispatch(removeCard(index));
+    dispatch(removeCard(index)); // Ta bort kortet från Redux storen
   };
 
   const handleCardClick = (index) => {
     const selectedCard = cards[index];
-    const updatedCards = cards.filter((card, i) => i !== index);
-    updatedCards.unshift(selectedCard);
-    dispatch(setSelectedCard(index));
-    dispatch(updateCardOrder(updatedCards));
+    const updatedCards = cards.filter((card, i) => i !== index); // Ta bort det valda kortet från listan
+    updatedCards.unshift(selectedCard); // Lägg till det valda kortet längst upp i listan
+    dispatch(setSelectedCard(index)); // Uppdatera det valda kortets index i storen
+    dispatch(updateCardOrder(updatedCards)); // Uppdatera ordningen av korten i storen
   };
 
   const getIcon = (cardDesign) => {
@@ -105,12 +104,12 @@ const Card = () => {
   };
 
   return (
-    <div className="card-container">
+    <div>
       {cards.map((card, index) => (
-        <div key={index} className={`card ${card.cardDesign}`} onClick={() => handleCardClick(index)}>
+        <div key={index} className={`card ${card.cardDesign}`} onClick={() => handleCardClick(index)}> {/* Lägg till onClick för att hantera klick på kortet */}
           <div className="logo-container">
             <img src={chipImage} className='chip-image' alt="" /> 
-            <img src={getIcon(card.cardDesign)} className='bitcoin' alt="" />
+            <img src={getIcon(card.cardDesign)} className='bitcoin' alt="" /> {/* Använd funktionen för att hämta rätt ikon */}
           </div>
           <div className="card-info">
             <p className="card-number">{card.number ? card.number : "XXXX XXXX XXXX XXXX"}</p>
@@ -119,7 +118,7 @@ const Card = () => {
               <p className="valid-thru">VALID THRU: <br /> {card.expiry ? card.expiry : " XX / XX"}</p>
             </div>
           </div>
-          <button onClick={() => handleRemoveCard(index)}>Remove</button>
+          <button onClick={() => handleRemoveCard(index)}>Radera</button> {/* Använd handleRemoveCard för att ta bort kortet */}
         </div>
       ))}
     </div>
