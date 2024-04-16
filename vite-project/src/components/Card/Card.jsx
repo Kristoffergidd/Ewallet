d2d43425318957515b79d8c8bb0253b6c6956bd1
Kristoffergidd <72502045+Kristoffergidd@users.noreply.github.com>
2024-04-16 11:02:16 +0200

    goat

--- vite-project/src/components/Card/Card.jsx
+++ vite-project/src/components/Card/Card.jsx
@@ -16,7 +16,12 @@ const Card = () => {
   };
 
   const handleCardClick = (index) => {
-    console.log(`Clicked on card with index ${index}`);
+    const selectedCard = cards[index]; // Spara det klickade kortet
+    const updatedCards = [...cards]; // Skapa en kopia av kortlistan
+    updatedCards.splice(index, 1); // Ta bort det klickade kortet från nuvarande position
+    updatedCards.unshift(selectedCard); // Lägg till det klickade kortet längst upp i listan
+    // Uppdatera kortens ordning i storen
+    dispatch({ type: 'card/updateCardOrder', payload: updatedCards });
   };
 
   const getIcon = (cardDesign) => {
@@ -59,66 +64,3 @@ const Card = () => {
 export default Card;
 
 
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { removeCard, setSelectedCard, updateCardOrder } from '../../reducers/cardSlice';
-// import vendorBitcoin from '../../assets/vendor-bitcoin.svg';
-// import vendorNinja from "../../assets/vendor-ninja.svg";
-// import chipImage from '../../assets/chip-dark.svg';
-// import vendorEvil from '../../assets/vendor-evil.svg';
-// import vendorBlockchain from '../../assets/vendor-blockchain.svg';
-
-// const Card = () => {
-//   const cards = useSelector(state => state.card.cards);
-//   const dispatch = useDispatch();
-
-//   const handleRemoveCard = (index) => {
-//     dispatch(removeCard(index)); // Ta bort kortet från Redux storen
-//   };
-
-//   const handleCardClick = (index) => {
-//     const selectedCard = cards[index];
-//     const updatedCards = cards.filter((card, i) => i !== index); // Ta bort det valda kortet från listan
-//     updatedCards.unshift(selectedCard); // Lägg till det valda kortet längst upp i listan
-//     dispatch(setSelectedCard(index)); // Uppdatera det valda kortets index i storen
-//     dispatch(updateCardOrder(updatedCards)); // Uppdatera ordningen av korten i storen
-//   };
-
-//   const getIcon = (cardDesign) => {
-//     switch (cardDesign) {
-//       case 'bitcoindesign':
-//         return vendorBitcoin;
-//       case 'ninja':
-//         return vendorNinja;
-//       case 'evil':
-//         return vendorEvil;
-//       case 'blockchain':
-//         return vendorBlockchain;
-//       default:
-//         return null;
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {cards.map((card, index) => (
-//         <div key={index} className={`card ${card.cardDesign}`} onClick={() => handleCardClick(index)}> {/* Lägg till onClick för att hantera klick på kortet */}
-//           <div className="logo-container">
-//             <img src={chipImage} className='chip-image' alt="" /> 
-//             <img src={getIcon(card.cardDesign)} className='bitcoin' alt="" /> {/* Använd funktionen för att hämta rätt ikon */}
-//           </div>
-//           <div className="card-info">
-//             <p className="card-number">{card.number ? card.number : "XXXX XXXX XXXX XXXX"}</p>
-//             <div className="cardholder-info">
-//               <p className="cardholder-name">CARDHOLDER NAME: <br /> {card.name}</p>
-//               <p className="valid-thru">VALID THRU: <br /> {card.expiry ? card.expiry : " XX / XX"}</p>
-//             </div>
-//           </div>
-//           <button onClick={() => handleRemoveCard(index)}>Radera</button> {/* Använd handleRemoveCard för att ta bort kortet */}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Card;
